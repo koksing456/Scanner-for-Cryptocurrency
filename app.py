7cd19bf48a2286598dfd6e2d52206bd273bd1684
koksing456 <koksing456@gmail.com>
2021-10-13 23:25:29 +0800

    update timeframe

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from flask import Flask, render_template
-from patterns import patterns
 import yfinance as yf
+import os, csv
+import pandas as pd
+import talib
+from datetime import date
+from flask import Flask, render_template, request
+from patterns import patterns
+
 
 app = Flask(__name__)
     
+
+    
 @app.route("/")
-def hello_world():
-    return render_template('index.html', patterns=patterns)
+def index():
+    pattern = request.args.get('pattern', None)
+    stocks = {}
+
+    with open('datasets/companies.csv') as f:
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company':row[1]}
+
+    print(stocks)        
+    if pattern:
+        datafiles = os.listdir('datasets/daily')
+        for filename in datafiles:
+            df = pd.read_csv('datasets/daily/{}'.format(filename))
+            pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
+
+            try:
+                result = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                last = result.tail(1).values[0]
+                print(last)
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
+            except:
+                pass
+        
+
+    return render_template('index.html', patterns=patterns, stocks=stocks, current_pattern = pattern)
 
 @app.route("/snapshot")
 def snapshot():
@@ -14,7 +50,7 @@ def snapshot():
         companies = f.read().splitlines()
         for company in companies:
             symbol = company.split(',')[0]
-            df = yf.download(symbol, start="2021-01-01", end="2021-08-01")
+            df = yf.download(symbol, start="2021-01-01", end=date.today)
             df.to_csv('datasets/daily/{}.csv'.format(symbol))
         
     return "ok"
